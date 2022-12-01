5c2e4fa9f62e606a9e9f5749dae5c869a48902e0
unsalbahadir <bbahadirunsal@gmail.com>
2022-12-01 10:11:12 +0100

    Add solution for challenge 1-2

--- src/solutions/day1/Solution.java
+++ src/solutions/day1/Solution.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Solution {
 
@@ -23,10 +24,32 @@ public class Solution {
         return maxTotalCalories;
     }
 
+    public int getMaxThreeTotalCalories(List<String> lines) {
+        PriorityQueue<Integer> maxThreeCalories = new PriorityQueue<>();
+
+        int currentTotal = 0;
+        for (String line : lines) {
+            if (line.equals("")) {
+                maxThreeCalories.add(currentTotal);
+                if (maxThreeCalories.size() > 3) {
+                    maxThreeCalories.poll();
+                }
+                currentTotal = 0;
+                continue;
+            }
+            currentTotal += Integer.parseInt(line);
+        }
+
+        return maxThreeCalories.stream()
+                .sorted()
+                .reduce(0, Integer::sum);
+    }
+
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
 
         List<String> lines = Files.readAllLines(Paths.get("inputs/day1input.txt"));
         System.out.println(solution.getMaxTotalCalories(lines));
+        System.out.println(solution.getMaxThreeTotalCalories(lines));
     }
 }
