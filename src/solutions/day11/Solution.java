3c78838ee401b684090d6767b8d35806c7feba1f
unsalbahadir <bbahadirunsal@gmail.com>
2022-12-11 22:15:10 +0100

    Refactor solutions for challenge 11-1 and 11-2

--- src/solutions/day11/Solution.java
+++ src/solutions/day11/Solution.java
@@ -10,50 +10,22 @@ import java.util.stream.Collectors;
 
 public class Solution {
 
-//    public int countLevelOfMonkeyBusiness(List<String> lines, int rounds, boolean divide) {
-//        List<Monkey> monkeys = createMonkeys(lines);
-//
-//        for (int i = 0; i < rounds; i++) {
-//            for (Monkey monkey : monkeys) {
-//                for (Iterator<Long> iterator = monkey.items.iterator(); iterator.hasNext(); ) {
-//                    Long item = iterator.next();
-//                    item = monkey.operation.apply(item);
-//                    if (divide) {
-//                        item /= 3;
-//                    }
-//                    boolean isDivisible = (item % monkey.divisibleBy) == 0;
-//
-//                    Monkey monkeyToThrowTo;
-//                    if (isDivisible) {
-//                        monkeyToThrowTo = monkeys.get(monkey.monkeyToThrowToIfTrue);
-//                    } else {
-//                        monkeyToThrowTo = monkeys.get(monkey.monkeyToThrowToIfFalse);
-//                    }
-//                    monkeyToThrowTo.items.add(item);
-//                    iterator.remove();
-//                    monkey.numberOfInspections++;
-//                }
-//            }
-//        }
-//        return monkeys.stream()
-//                .map(monkey -> monkey.numberOfInspections)
-//                .sorted(Collections.reverseOrder())
-//                .limit(2)
-//                .reduce(1, (integer, integer2) -> integer * integer2);
-//    }
-
-    public Long countLevelOfMonkeyBusiness2(List<String> lines) {
+    public Long countLevelOfMonkeyBusiness(List<String> lines, int rounds, boolean divide) {
         List<Monkey> monkeys = createMonkeys(lines);
 
         int dividedByTotalProduct = monkeys.stream()
                 .map(monkey -> monkey.divisibleBy)
                 .reduce(1, (integer, integer2) -> integer * integer2);
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < rounds; i++) {
             for (Monkey monkey : monkeys) {
                 for (Iterator<BigInteger> iterator = monkey.items.iterator(); iterator.hasNext(); ) {
                     BigInteger item = iterator.next();
                     item = monkey.operation.apply(item);
-                    item = item.remainder(BigInteger.valueOf(dividedByTotalProduct));
+                    if (divide) {
+                        item = item.divide(BigInteger.valueOf(3));
+                    } else {
+                        item = item.remainder(BigInteger.valueOf(dividedByTotalProduct));
+                    }
                     boolean isDivisible = (item.remainder(BigInteger.valueOf(monkey.divisibleBy)).equals(BigInteger.ZERO));
 
                     Monkey monkeyToThrowTo;
@@ -152,7 +124,7 @@ public class Solution {
         Solution solution = new Solution();
 
         List<String> lines = Files.readAllLines(Paths.get("inputs/day11input.txt"));
-//        System.out.println(solution.countLevelOfMonkeyBusiness(lines, 20, true));
-        System.out.println(solution.countLevelOfMonkeyBusiness2(lines));
+        System.out.println(solution.countLevelOfMonkeyBusiness(lines, 20, true));
+        System.out.println(solution.countLevelOfMonkeyBusiness(lines, 10000, false));
     }
 }
