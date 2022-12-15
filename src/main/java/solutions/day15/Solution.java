38349c583bda6f3299a83b7904f949d1249b17e9
unsalbahadir <bbahadirunsal@gmail.com>
2022-12-15 11:38:58 +0100

    Add solution for challenge 15-2

--- src/main/java/solutions/day15/Solution.java
+++ src/main/java/solutions/day15/Solution.java
@@ -118,10 +118,71 @@ public class Solution {
         }
     }
 
+    public long getSolution2(List<String> lines) {
+        build(lines);
+        Position beaconPosition = searchBeacon();
+
+        if (beaconPosition == null) {
+            return -1;
+        }
+        return (beaconPosition.column * 4000000L) + beaconPosition.row;
+    }
+
+    private Position searchBeacon() {
+        for (Position sensorPosition : sensorPositions) {
+            // get fringe positions on each row
+            int distanceToClosestBeacon = sensorPosition.distanceToClosestBeacon;
+            for (int i = -distanceToClosestBeacon; i <= distanceToClosestBeacon; i++) {
+                int row = sensorPosition.row + i;
+                if (row < 0 || row > 4000000) {
+                    continue;
+                }
+                int horizontalDistanceLeft = distanceToClosestBeacon - Math.abs(i);
+                Position rightmostPosition = new Position(row, sensorPosition.column + horizontalDistanceLeft + 1);
+                Position leftmostPosition = new Position(row, sensorPosition.column - horizontalDistanceLeft - 1);
+
+                Position position = checkPositions(List.of(rightmostPosition, leftmostPosition));
+                if (position != null) {
+                    return position;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private Position checkPositions(List<Position> positions) {
+        for (Position position : positions) {
+            if (isTargetBeacon(position)) {
+                return position;
+            }
+        }
+        return null;
+    }
+
+    private boolean isTargetBeacon(Position position) {
+        int limit = 4000000;
+        return position.row >= 0 && position.column >= 0
+                && position.row <= limit && position.column <= limit
+                && !sensorPositions.contains(position)
+                && !beaconPositions.contains(position)
+                && notInDistanceOfAnySensor(position);
+    }
+
+    private boolean notInDistanceOfAnySensor(Position position) {
+        return sensorPositions.stream()
+                .noneMatch(sensorPosition -> isInDistanceToSensor(position, sensorPosition));
+    }
+
+    private boolean isInDistanceToSensor(Position currentPosition, Position sensorPosition) {
+        return getManhattanDistance(currentPosition, sensorPosition) <= sensorPosition.distanceToClosestBeacon;
+    }
+
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
 
         List<String> lines = Files.readAllLines(Paths.get("inputs/day15input.txt"));
-        System.out.println(solution.getSolution(lines));
+//        System.out.println(solution.getSolution(lines));
+        System.out.println(solution.getSolution2(lines));
     }
 }
